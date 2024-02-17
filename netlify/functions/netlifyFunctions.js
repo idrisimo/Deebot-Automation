bb4a317cc76a58308c77c8384da5f8e9357981be
idrisimo <idrissilva@hotmail.com>
2024-02-17 02:22:59 +0000

    another

--- netlify/functions/netlifyFunctions.js
+++ netlify/functions/netlifyFunctions.js
@@ -1,4 +1,4 @@
-const EcoVacsControl = require('../../../EcoVacsControl');
+const EcoVacsControl = require('../EcoVacsControl');
 
 exports.handler = async (event, context) => {
     const ecoVacsControl = new EcoVacsControl();
